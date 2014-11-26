da92b37b0a6631c13888ce3d66f7c7b902e6a6dd
Tyler Madison <tdmadison@gmail.com>
2014-11-26 12:28:49 -0800

    at a good place, got charts 99% matching gabes comps, and have overlay toggling with dat GUI

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
-var $ = require('jquery');
-var utils = require('./utils');
-var hardCodedTwitterUserForTestingLocally = 'gaberuane';
+require('../js/vendor/dat.gui.min.js');
+
+var $ = require('jquery'),
+  utils = require('./utils'),
+  hardCodedTwitterUserForTestingLocally = 'tylermadison',
+  locStr = window.location.href.toString(),
+  user = locStr.substr(locStr.indexOf('@') + 1);
+
+if (user.length) {
+  hardCodedTwitterUserForTestingLocally = user;
+}
+console.log(user);
 
 var drawConfig = {
   radius: 335,
@@ -10,7 +19,44 @@ var drawConfig = {
   centerY: 325
 };
 
-function renderCharCountChart(ctx, dataObj) {
+// When data parses store in hash
+var models = {};
+
+var DatGUIConfig = function() {
+  this.showMerged = false;
+}
+
+var Point = function(x, y) {
+  this.x = x || 0;
+  this.y = y || 0;
+}
+
+function setupGUI() {
+  var config = new DatGUIConfig(),
+    datGUI = new dat.GUI();
+  (datGUI.add(config, 'showMerged')).onChange(function(newVal) {
+    renderCharCountChart(document.getElementById('charater-counts').getContext('2d'), models.charCount, !newVal);
+    renderTimeOfDayChart(document.getElementById('time-of-day').getContext('2d'), models.timeOfDay, !newVal);
+    renderMostUsedCharacterChart(document.getElementById('most-used').getContext('2d'), models.mostUsedChar, !newVal);
+
+    $('#charater-counts, #time-of-day, #most-used, #most-used-markers').toggleClass('merged')
+
+    if (newVal === true) {
+      var ctx = document.getElementById('most-used').getContext('2d');
+      ctx.font = '14pt HelveticaNeue-Light';
+      ctx.fillStyle = '#76787A';
+      ctx.textAlign = 'left';
+      ctx.fillText('@' + hardCodedTwitterUserForTestingLocally, 15, 30);
+      ctx.font = '9pt HelveticaNeue-Light';
+      ctx.fillStyle = '#ffffff';
+      ctx.textAlign = 'center';
+      ctx.fillText('Layering of all three - looks pretty cool, just sayin.', 250, 630);
+    }
+
+  });
+}
+
+function renderCharCountChart(ctx, dataObj, renderOutlines) {
   var numPoints = Object.keys(dataObj).length,
     angleIncrement = (360 / numPoints),
     rad = Math.PI / 180;
@@ -18,6 +64,8 @@ function renderCharCountChart(ctx, dataObj) {
   var i = 0,
     mult = utils.getDistMult(dataObj, drawConfig.radius * 0.5),
     minOffset = 45;
+
+  ctx.clearRect(0, 0, 500, 650);
 
   ctx.lineWidth = 1.5;
   ctx.strokeStyle = '#EC0972';
@@ -43,72 +91,86 @@ function renderCharCountChart(ctx, dataObj) {
   ctx.fill();
 
   i = 0;
-  ctx.font = '6pt HelveticaNeue-Light';
+  ctx.font = '8pt HelveticaNeue-Light';
   ctx.fillStyle = '#76787A';
   ctx.strokeStyle = '#76787A';
   ctx.textAlign = 'center';
   ctx.lineWidth = 1;
 
   // Draw tick marks around circumference
-  for (var i = 0; i < numPoints; i++) {
-    ctx.beginPath();
-    var circRadius = 222,
-      angleStep = (angleIncrement * i - 88),
-      xx = drawConfig.centerX + circRadius * Math.cos(angleStep * rad),
-      yy = drawConfig.centerY + circRadius * Math.sin(angleStep * rad),
-      xxx = drawConfig.centerX + (circRadius + 10) * Math.cos(angleStep * rad),
-      yyy = drawConfig.centerY + (circRadius + 10) * Math.sin(angleStep * rad),
-      textX = drawConfig.centerX + (circRadius + 20) * Math.cos(angleStep * rad),
-      textY = drawConfig.centerY + 3 + (circRadius + 20) * Math.sin(angleStep * rad);
-    // Place times ever 10th character
-    if (i % 10 == 9) {
-      ctx.fillText(i + 1, textX, textY);
-      ctx.strokeStyle = '#ffffff';
-    } else {
-      ctx.strokeStyle = '#76787A';
+  //
+  if (renderOutlines) {
+    for (var i = 0; i < numPoints; i++) {
+      ctx.beginPath();
+      var circRadius = 222,
+        angleStep = (angleIncrement * i - 88),
+        xx = drawConfig.centerX + circRadius * Math.cos(angleStep * rad),
+        yy = drawConfig.centerY + circRadius * Math.sin(angleStep * rad),
+        xxx = drawConfig.centerX + (circRadius + 10) * Math.cos(angleStep * rad),
+        yyy = drawConfig.centerY + (circRadius + 10) * Math.sin(angleStep * rad),
+        textX = drawConfig.centerX + (circRadius + 20) * Math.cos(angleStep * rad),
+        textY = drawConfig.centerY + 3 + (circRadius + 20) * Math.sin(angleStep * rad);
+      // Place times ever 10th character
+      if (i % 10 == 9) {
+        ctx.fillText(i + 1, textX, textY);
+        ctx.strokeStyle = '#ffffff';
+      } else {
+        ctx.strokeStyle = '#76787A';
+      }
+
+      ctx.moveTo(xx, yy);
+      ctx.lineTo(xxx, yyy);
+      ctx.stroke();
     }
 
-    ctx.moveTo(xx, yy);
-    ctx.lineTo(xxx, yyy);
-    ctx.stroke();
+    ctx.font = '9pt HelveticaNeue-Light';
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = 'center';
+    ctx.fillText('Graph A: character count within a tweet', 250, 630);
+
+    // Filltext user name
+    var ctx = document.getElementById('charater-counts').getContext('2d');
+    ctx.font = '14pt HelveticaNeue-Light';
+    ctx.fillStyle = '#76787A';
+    ctx.textAlign = 'left';
+    ctx.fillText('@' + hardCodedTwitterUserForTestingLocally, 15, 30);
   }
-
-  ctx.font = '9pt HelveticaNeue-Light';
-  ctx.fillStyle = '#ffffff';
-  ctx.textAlign = 'center';
-  ctx.fillText('Graph A: character count within a tweet', 250, 630);
-
-  // Filltext user name
-  var ctx = document.getElementById('charater-counts').getContext('2d');
-  ctx.font = '14pt HelveticaNeue-Light';
-  ctx.fillStyle = '#76787A';
-  ctx.textAlign = 'left';
-  ctx.fillText('@' + hardCodedTwitterUserForTestingLocally, 15, 30);
 
 }
 
-function renderTimeOfDayChart(ctx, dataObj) {
+function renderTimeOfDayChart(ctx, dataObj, renderOutlines) {
   var numPoints = Object.keys(dataObj).length,
     angleIncrement = (360 / numPoints),
     rad = Math.PI / 180;
+
+  var markers = [];
 
   var i = 0,
     startX = -1,
     startY = -1,
     lastX = -1,
     lastY = -1,
-    mult = utils.getDistMult(dataObj, drawConfig.radius - 215),
+    mult = utils.getDistMult(dataObj, drawConfig.radius - 225),
     minOffset = 45; // Inner circle
 
-  console.log('Using Multiplyer :: ', mult);
+  // console.log('Using Multiplyer :: ', mult);
+
+  ctx.clearRect(0, 0, 500, 650);
 
   ctx.beginPath();
 
+  //console.log('Rendering time of day: ', dataObj);
+
+  var highestVal = -1,
+    highestPoint = null;
+
   for (var key in dataObj) {
     var amount = dataObj[key],
-      angleStep = (angleIncrement * i - 75),
+      angleStep = (angleIncrement * i - 90),
       currX = ((mult * amount + minOffset) * Math.cos(angleStep * rad)),
       currY = ((mult * amount + minOffset) * Math.sin(angleStep * rad));
+
+    //console.log(key, '==>', amount);
 
     if (startX === -1 && startY === -1) {
       startX = drawConfig.centerX + currX;
@@ -123,71 +185,100 @@ function renderTimeOfDayChart(ctx, dataObj) {
       ctx.lineTo(startX, startY); // Gets back to home to fill
     } else {
       if (i === 0) {
-        ctx.moveTo(drawConfig.centerX + currX, drawConfig.centerY + currY);
-        ctx.lineTo(drawConfig.centerY + nextX, drawConfig.centerY + nextY);
 
         lastX = drawConfig.centerX + nextX;
         lastY = drawConfig.centerY + nextY;
+
+        ctx.moveTo(drawConfig.centerX + currX, drawConfig.centerY + currY);
+        ctx.lineTo(lastX, lastY);
+
+        // push new point to mark
+        markers.push(new Point(drawConfig.centerX + currX, drawConfig.centerY + currY));
 
       } else {
-        ctx.lineTo(drawConfig.centerX + nextX, drawConfig.centerY + nextY);
         lastX = drawConfig.centerX + nextX;
         lastY = drawConfig.centerY + nextY;
+
+        ctx.lineTo(lastX, lastY);
+
+        // push new point to mark
+        markers.push(new Point(lastX, lastY));
+
       }
 
+    }
+
+    if (amount > highestVal) {
+      highestVal = amount;
+      highestPoint = new Point(drawConfig.centerX + currX, drawConfig.centerY + currY);
     }
 
     i++;
   }
 
   ctx.closePath();
-
   ctx.fillStyle = 'rgba(0, 173, 239, 0.5)';
   ctx.fill();
 
+  // Fill center
+  /*  ctx.beginPath();
+    ctx.fillStyle = 'rgba(39, 54, 63, 1)';
+    ctx.moveTo(drawConfig.centerX, drawConfig.centerY);
+    ctx.arc(drawConfig.centerX, drawConfig.centerY, 40, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.fill();*/
+
   // Draw tick marks around circumference
-  for (var i = 0; i < numPoints; i++) {
-    ctx.beginPath();
-    var circRadius = 180,
-      angleStep = (angleIncrement * i - 75),
-      xx = drawConfig.centerX + circRadius * Math.cos(angleStep * rad),
-      yy = drawConfig.centerY + circRadius * Math.sin(angleStep * rad),
-      xxx = drawConfig.centerX + (circRadius + 10) * Math.cos(angleStep * rad),
-      yyy = drawConfig.centerY + (circRadius + 10) * Math.sin(angleStep * rad),
-      textX = drawConfig.centerX + (circRadius + 20) * Math.cos(angleStep * rad),
-      textY = drawConfig.centerY + 3 + (circRadius + 20) * Math.sin(angleStep * rad);
-    // Place times ever 10th character
-    if (i % 6 == 5) {
-      ctx.textAlign = 'center';
-      ctx.fillStyle = '#76787A';
-      ctx.fillText(i + 1, textX, textY);
-      ctx.strokeStyle = '#ffffff';
-    } else {
-      ctx.strokeStyle = '#76787A';
+  if (renderOutlines) {
+    for (var i = 0; i < numPoints; i++) {
+      ctx.beginPath();
+      var circRadius = 180,
+        angleStep = (angleIncrement * i - 75),
+        xx = drawConfig.centerX + circRadius * Math.cos(angleStep * rad),
+        yy = drawConfig.centerY + circRadius * Math.sin(angleStep * rad),
+        xxx = drawConfig.centerX + (circRadius + 10) * Math.cos(angleStep * rad),
+        yyy = drawConfig.centerY + (circRadius + 10) * Math.sin(angleStep * rad),
+        textX = drawConfig.centerX + (circRadius - 12) * Math.cos(angleStep * rad),
+        textY = drawConfig.centerY + 3 + (circRadius - 12) * Math.sin(angleStep * rad);
+      // Place times ever 10th character
+      if (i % 6 == 5) {
+        ctx.font = '8pt HelveticaNeue-Light';
+        ctx.textAlign = 'center';
+        ctx.fillStyle = '#76787A';
+        ctx.fillText(i + 1, textX, textY);
+        ctx.strokeStyle = '#ffffff';
+      } else {
+        ctx.strokeStyle = '#76787A';
+      }
+
+      ctx.moveTo(xx, yy);
+      ctx.lineTo(xxx, yyy);
+      ctx.stroke();
     }
-    ctx.moveTo(xx, yy);
-    ctx.lineTo(xxx, yyy);
-    ctx.stroke();
+    ctx.font = '9pt HelveticaNeue-Light';
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = 'center';
+    ctx.fillText('Graph B: time of day', 250, 630);
+
+    ctx.font = '14pt HelveticaNeue-Light';
+    ctx.fillStyle = '#76787A';
+    ctx.textAlign = 'left';
+    ctx.fillText('@' + hardCodedTwitterUserForTestingLocally, 15, 30);
   }
 
-  ctx.font = '9pt HelveticaNeue-Light';
-  ctx.fillStyle = '#ffffff';
-  ctx.textAlign = 'center';
-  ctx.fillText('Graph B: time of day', 250, 630);
-
-  ctx.font = '14pt HelveticaNeue-Light';
-  ctx.fillStyle = '#76787A';
-  ctx.textAlign = 'left';
-  ctx.fillText('@' + hardCodedTwitterUserForTestingLocally, 15, 30);
+  drawTrianlgeMarker(highestPoint, ctx);
+  //console.log('highestVal:', highestVal, highestPoint);
 
 }
 
-function renderMostUsedCharacterChart(ctx, dataObj) {
+function renderMostUsedCharacterChart(ctx, dataObj, renderOutlines) {
   var chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '?', '/', '#', '$', '%', '&', '*', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '.', ',', ':', '"'];
 
   var numPoints = chars.length,
     angleIncrement = (360 / numPoints),
     rad = Math.PI / 180;
+
+  var markers = models.mostUsedMarkers = [];
 
   var i = 0,
     startX = -1,
@@ -196,6 +287,8 @@ function renderMostUsedCharacterChart(ctx, dataObj) {
     lastY = -1,
     mult = utils.getDistMult(dataObj, drawConfig.radius * 0.5),
     minOffset = 45; // Inner circle
+
+  ctx.clearRect(0, 0, 500, 650);
 
   ctx.beginPath();
 
@@ -206,7 +299,7 @@ function renderMostUsedCharacterChart(ctx, dataObj) {
       currX = ((mult * amount + minOffset) * Math.cos(angleStep * rad)),
       currY = ((mult * amount + minOffset) * Math.sin(angleStep * rad));
 
-    console.log('plotting: ', chars[i], dataObj[chars[i]] || 0);
+    //console.log('plotting: ', chars[i], dataObj[chars[i]] || 0);
 
     if (startX === -1 && startY === -1) {
       startX = drawConfig.centerX + currX;
@@ -221,21 +314,18 @@ function renderMostUsedCharacterChart(ctx, dataObj) {
       ctx.lineTo(startX, startY); // Gets back to home to fill
     } else {
       if (i === 0) {
-
+        lastX = drawConfig.centerX + nextX;
+        lastY = drawConfig.centerY + nextY;
         ctx.moveTo(drawConfig.centerX + currX, drawConfig.centerY + currY);
-        ctx.lineTo(drawConfig.centerY + nextX, drawConfig.centerY + nextY);
-
-        lastX = drawConfig.centerX + nextX;
-        lastY = drawConfig.centerY + nextY;
-
+        ctx.lineTo(lastX, lastY);
+        markers.push(new Point(lastX, lastY));
       } else {
-        ctx.lineTo(drawConfig.centerX + nextX, drawConfig.centerY + nextY);
         lastX = drawConfig.centerX + nextX;
         lastY = drawConfig.centerY + nextY;
+        ctx.lineTo(lastX, lastY);
+        markers.push(new Point(lastX, lastY));
       }
-
     }
-
   }
 
   ctx.closePath();
@@ -246,39 +336,82 @@ function renderMostUsedCharacterChart(ctx, dataObj) {
   ctx.fillStyle = '#76787A';
   ctx.beginPath();
 
-  for (var i = 0; i < chars.length; i++) {
+  if (renderOutlines) {
+    for (var i = 0; i < chars.length; i++) {
+      var circRadius = 222,
+        angleStep = (angleIncrement * i - 90),
+        xx = drawConfig.centerX + circRadius * Math.cos(angleStep * rad),
+        yy = drawConfig.centerY + circRadius * Math.sin(angleStep * rad),
+        xxx = drawConfig.centerX + (circRadius + 10) * Math.cos(angleStep * rad),
+        yyy = drawConfig.centerY + (circRadius + 10) * Math.sin(angleStep * rad),
+        textX = drawConfig.centerX + (circRadius + 20) * Math.cos(angleStep * rad),
+        textY = drawConfig.centerY + 3 + (circRadius + 20) * Math.sin(angleStep * rad);
+      // Place times ever 10th character
 
-    var circRadius = 222,
-      angleStep = (angleIncrement * i - 90),
-      xx = drawConfig.centerX + circRadius * Math.cos(angleStep * rad),
-      yy = drawConfig.centerY + circRadius * Math.sin(angleStep * rad),
-      xxx = drawConfig.centerX + (circRadius + 10) * Math.cos(angleStep * rad),
-      yyy = drawConfig.centerY + (circRadius + 10) * Math.sin(angleStep * rad),
-      textX = drawConfig.centerX + (circRadius + 20) * Math.cos(angleStep * rad),
-      textY = drawConfig.centerY + 3 + (circRadius + 20) * Math.sin(angleStep * rad);
-    // Place times ever 10th character
+      ctx.textAlign = 'center';
 
+      if (i < 26 || i > 33 && i < 44) {
+        ctx.fillStyle = '#ffffff';
+      } else {
+        ctx.fillStyle = '#76787A';
+      }
+      ctx.font = '8pt HelveticaNeue-Light';
+      ctx.fillText(chars[i].toUpperCase(), textX, textY);
+      ctx.strokeStyle = '#76787A';
+
+      ctx.moveTo(xx, yy);
+      ctx.lineTo(xxx, yyy);
+
+    }
+    ctx.font = '9pt HelveticaNeue-Light';
+    ctx.fillStyle = '#ffffff';
     ctx.textAlign = 'center';
+    ctx.fillText('Graph C: most used character', 250, 630);
 
-    ctx.fillText(chars[i].toUpperCase(), textX, textY);
-    ctx.strokeStyle = '#76787A';
+    ctx.font = '14pt HelveticaNeue-Light';
+    ctx.fillStyle = '#76787A';
+    ctx.textAlign = 'left';
+    ctx.fillText('@' + hardCodedTwitterUserForTestingLocally, 15, 30);
 
-    ctx.moveTo(xx, yy);
-    ctx.lineTo(xxx, yyy);
+    // Draw the markers
+    drawMarkers(models.mostUsedMarkers, ctx);
 
+    var mostUsedCtx = document.getElementById('most-used-markers').getContext('2d');
+    mostUsedCtx.clearRect(0, 0, 500, 650);
+    drawMarkers(models.mostUsedMarkers, mostUsedCtx);
   }
 
   ctx.stroke();
 
-  ctx.font = '9pt HelveticaNeue-Light';
-  ctx.fillStyle = '#ffffff';
-  ctx.textAlign = 'center';
-  ctx.fillText('Graph C: most used character', 250, 630);
+}
 
-  ctx.font = '14pt HelveticaNeue-Light';
-  ctx.fillStyle = '#76787A';
-  ctx.textAlign = 'left';
-  ctx.fillText('@' + hardCodedTwitterUserForTestingLocally, 15, 30);
+function drawMarkers(points, ctx) {
+
+  for (var i = 0; i < points.length; i++) {
+    var point = points[i];
+    //console.log('drawing point', point.x, point.y);
+    ctx.beginPath();
+    ctx.strokeStyle = 'rgba(3, 161, 220, 1)';
+    ctx.arc(point.x, point.y, 4, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+function drawTrianlgeMarker(point, ctx) {
+  var tW = 6,
+    tH = 6;
+
+  ctx.beginPath();
+  ctx.strokeStyle = 'rgba(3, 161, 220, 1)';
+  ctx.fillStyle = '#ffffff';
+  ctx.lineWidth = 1.5;
+  ctx.moveTo(point.x - tW, point.y + 3);
+  ctx.lineTo(point.x + 0, point.y - tH);
+  ctx.lineTo(point.x + tW, point.y + 3);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
 }
 
 /**
@@ -357,13 +490,14 @@ function parseData(data) {
     }
   }
 
-  // Render time of day chart to the timeof day canvas element
-  renderCharCountChart(document.getElementById('charater-counts').getContext('2d'), charCount);
-  renderTimeOfDayChart(document.getElementById('time-of-day').getContext('2d'), timeOfDay);
-  renderMostUsedCharacterChart(document.getElementById('most-used').getContext('2d'), mostUsedChar);
+  models['charCount'] = charCount;
+  models['timeOfDay'] = timeOfDay
+  models['mostUsedChar'] = mostUsedChar;
 
-  // Done parsing
-  //console.log('Time of day:', timeOfDay, 'Character Count:', charCount, 'Most Used Character:', mostUsedChar);
+  // Render time of day chart to the timeof day canvas element
+  renderCharCountChart(document.getElementById('charater-counts').getContext('2d'), models.charCount, true);
+  renderTimeOfDayChart(document.getElementById('time-of-day').getContext('2d'), models.timeOfDay, true);
+  renderMostUsedCharacterChart(document.getElementById('most-used').getContext('2d'), models.mostUsedChar, true);
 
 }
 
@@ -372,6 +506,8 @@ function init() {
   promise.then(function(data) {
     parseData(data);
   });
+
+  setupGUI();
 }
 
 $(init);
