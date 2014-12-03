40ebd511adda0d3488f5ac5142c5eca02021769d
Tyler Madison <tdmadison@gmail.com>
2014-12-03 14:42:25 -0800

    getting up on herokussss

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ server.get('/', function(req, res) {
     root: __dirname + '/dist'
   });*/
 
-  res.send(__dirname);
+  res.sendFile('index.html', { root: path.join(__dirname, '../dist') });
 });
 
 server.listen(server.get('port'), function() {
